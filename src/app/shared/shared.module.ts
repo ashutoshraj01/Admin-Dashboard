240f9bf9c040bfbdbcbb6f6f1e53d71a42a0bb12
Ashutosh Raj <ashutoshraj908@gmail.com>
2020-11-27 01:47:25 +0530

    Basic structuring done

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,8 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-
-
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
   declarations: [
@@ -13,7 +17,13 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
     SidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatMenuModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   exports: [
     HeaderComponent,
